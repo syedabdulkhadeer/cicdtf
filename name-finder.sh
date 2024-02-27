2bef1585282e6eedd74563e772d0b93714af8f84
syedabdulkhadeer <khadeera714@gmail.com>
2024-02-27 11:13:40 +0530

    modified few files

--- name-finder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-identifier_file= `cat users.txt | grep Kha.eer`
-echo "output:$identifier_file"
---
+++ testfile1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+identifier_file=`cat users.txt | grep Kha.eer`
+
+echo "output:"
+
+echo "$identifier_file"
