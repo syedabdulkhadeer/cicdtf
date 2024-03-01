b7443a3eb0462558fb302c817dbb810204d9ee9f
syedabdulkhadeer <khadeera714@gmail.com>
2024-03-01 12:03:54 +0530

    added few lines to the file test1.sh

--- test1.sh
+++ test1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Creating the env variable
-my_variable="There is some value here"
+my_variable="This is khadeer"
 
 # Appling if condition with -z which is to check the string is empty or not
 
 if [ -z "$my_variable" ]; then
-	echo "There is some value in given variable"
+	echo "There is no value in given variable: $my_variable"
 else
-	echo "There is no value in given variable"
+	echo "There is some value in given variable: $my_variable"
 fi
 
